7001b33c04bcc743e8d6a387952e18ac5ac4b755
Gabriel <geraldg652@gmail.com>
2023-06-13 17:35:18 +0300

    added contact-us form

--- index.js
+++ index.js
@@ -1,84 +1,89 @@
-var heading = document.getElementById('home-text');
-var description = document.getElementById('home-description');
-var img = document.getElementById('actual-image');
+let heading = document.getElementById('home-text');
+let description = document.getElementById('home-description');
+let img = document.getElementById('actual-image');
+if (heading != null && description != null && img != null) {
+    let headings = [
+        {
+            title: "Unleash ",
+            subtitle: "Optimal Care: ",
+            title2: "Where Health Meets Hooves, Paws, and Whiskers"
+        },
+        {
+            title: "Care for Your Beloved ",
+            subtitle: "Pets",
+            title2: ""
+        },
+        {
+            title: "Expert Care for ",
+            subtitle: "Cattle, Sheep, and Goats",
+            title2: ""
+        }
+    ];
 
-var headings = [
-    {
-        title: "Unleash ",
-        subtitle: "Optimal Care: ",
-        title2: "Where Health Meets Hooves, Paws, and Whiskers"
-    },
-    {
-        title: "Care for Your Beloved ",
-        subtitle: "Pets",
-        title2: ""
-    },
-    {
-        title: "Expert Care for ",
-        subtitle: "Cattle, Sheep, and Goats",
-        title2: ""
+    let descriptions = [
+        `From cats and dogs to exotic pets and farm animals, we provide comprehensive care for every all.`,
+        `Experience top-notch veterinary services at our state-of-the-art clinic. We are dedicated to ensuring the health and happiness of your furry companions. Our team of experienced professionals is committed to providing the highest level of care for every pet that walks through our doors.`,
+        `Specialized in providing expert care for cattle, sheep, and goats. From routine check-ups to advanced medical treatments, our experienced veterinarians are dedicated to ensuring the health and well-being of your livestock.`
+    ];
+
+    let images = ['./images/home-images/IMG-20171017-WA0008.jpg', './images/home-images/cat_and_dog.avif', './images/gallery-images/dairycow.jpg'];
+
+    // -- preloading the images -- //
+    let imageObjects = []
+    for (let i = 0; i < images.length; i++) {
+        let imageObj = new Image()
+        imageObj.src = images[i]
+        imageObjects.push(imageObj)
     }
-];
 
-var descriptions = [
-    `From cats and dogs to exotic pets and farm animals, we provide comprehensive care for every all.`,
-    `Experience top-notch veterinary services at our state-of-the-art clinic. We are dedicated to ensuring the health and happiness of your furry companions. Our team of experienced professionals is committed to providing the highest level of care for every pet that walks through our doors.`,
-    `Specialized in providing expert care for cattle, sheep, and goats. From routine check-ups to advanced medical treatments, our experienced veterinarians are dedicated to ensuring the health and well-being of your livestock.`
-];
+    let counter = 0;
 
-var images = ['./images/home-images/IMG-20171017-WA0008.jpg', './images/home-images/cat_and_dog.avif', './images/gallery-images/dairycow.jpg'];
+    const changeContent = () => {
+        if (counter >= images.length) {
+            counter = 0
+        }
+        heading.innerHTML = `<h3>${headings[counter].title}<span>${headings[counter].subtitle}</span>${headings[counter].title2}</h3>`;
+        description.innerHTML = descriptions[counter];
+        img.src = images[counter]
 
-// -- preloading the images -- //
-var imageObjects = []
-for (let i = 0; i < images.length; i++) {
-    var imageObj = new Image()
-    imageObj.src = images[i]
-    imageObjects.push(imageObj)
-}
-
-var counter = 0;
-
-const changeContent = () => {
-    if (counter >= images.length) {
-        counter = 0
+        counter++
+        setTimeout(changeContent, 7000)
     }
-    heading.innerHTML = `<h3>${headings[counter].title}<span>${headings[counter].subtitle}</span>${headings[counter].title2}</h3>`;
-    description.innerHTML = descriptions[counter];
-    img.src = images[counter]
 
-    counter++
-    setTimeout(changeContent, 7000)
 }
 
 
 // --- Hamburger Menu
 const hamburger = document.querySelector(".hamburger")
 const navMenu = document.querySelector(".nav-menu")
-
-hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active")
-    navMenu.classList.toggle("active")
-})
-
-document.querySelectorAll(".nav-link").forEach(n => {
-    n.addEventListener("click", () => {
-        hamburger.classList.remove("active")
-        navMenu.classList.remove("active")
+if (hamburger != null && navMenu != null) {
+    hamburger.addEventListener("click", () => {
+        hamburger.classList.toggle("active")
+        navMenu.classList.toggle("active")
     })
-})
+
+    document.querySelectorAll(".nav-link").forEach(n => {
+        n.addEventListener("click", () => {
+            hamburger.classList.remove("active")
+            navMenu.classList.remove("active")
+        })
+    })
+
+}
 
 // --- hide navbar --- //
-var lastScrollTop
+let lastScrollTop
+let navbar = document.getElementById('navbar')
+if (navbar != null) {
+    window.addEventListener('scroll', function () {
+        let scrollTop = window.pageYOffset || document.doctype.scrollTop
 
-var navbar = document.getElementById('navbar')
+        if (scrollTop > lastScrollTop) {
+            navbar.style.top = '-90px'
+        } else {
+            navbar.style.top = '0'
+        }
+        lastScrollTop = scrollTop
+    })
 
-window.addEventListener('scroll', function () {
-    var scrollTop = window.pageYOffset || document.doctype.scrollTop
-
-    if (scrollTop > lastScrollTop) {
-        navbar.style.top = '-90px'
-    } else {
-        navbar.style.top = '0'
-    }
-    lastScrollTop = scrollTop
-})
+}
