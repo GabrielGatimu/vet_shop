5275b971b82e0915a7fe071da480f4a9ed540da3
GabrielGatimu <geraldg652@gmail.com>
2023-06-21 16:27:35 +0300

    fixed all nav-links

--- index.js
+++ index.js
@@ -57,37 +57,37 @@
 
 var slideIndex = 0;
 showSlides(); // call showslide method
- 
+
 function showSlides() {
     var i;
- 
+
     var slides = document.getElementsByClassName("slider-fade");
- 
+
     // var dots = document.getElementsByClassName("dot");
- 
+
     for (i = 0; i < slides.length; i++) {
         // initially set the display to
         // none for every image.
         slides[i].style.display = "none";
     }
- 
+
     // increase by 1, Global variable
     slideIndex++;
- 
+
     // check for boundary
     if (slideIndex > slides.length) {
         slideIndex = 1;
     }
- 
+
     // for (i = 0; i < dots.length; i++) {
     //     dots[i].className = dots[i].className.replace(" active", "");
     // }
- 
-    slides[slideIndex - 1].style.display = "block";
-  ///  dots[slideIndex - 1].className += " active";
- 
+
+    slides[slideIndex - 1].style.display = "flex";
+    ///  dots[slideIndex - 1].className += " active";
+
     // Change image every 3 seconds
-    setTimeout(showSlides, 3000);
+    setTimeout(showSlides, 5000);
 }
 
 const hamburger = document.querySelector("#hamburger")
